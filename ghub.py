6a38ce8e56fa31a0496ef1862de4b4f5c20d63ec
bmaca <novmaca@gmail.com>
2018-01-01 23:48:16 -0600

    change_color_to_blue

--- ghub.py
+++ ghub.py
@@ -419,7 +419,7 @@ def print_pull_request(pr, verbose, reviews=None):
         print_pull_request_comments(pr['number'])
     else:
         assignee = "[{}]".format(
-            colored(pr['assignee']['login'], 'yellow', ['bold'])) if pr.get('assignee') else ""
+            colored(pr['assignee']['login'], 'blue', ['bold'])) if pr.get('assignee') else ""
         print_tuple(pr['user']['login'][:12],
                     wrap_to_console('#%s %s %s' % (pr['number'], pr['title'], assignee))[0],
                     a_color='cyan')
